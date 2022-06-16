291a57e942ffebce118d0651c9c3ccf5906fc5bb
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-06-17 00:11:52 +0600

    database added

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.harcewd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
+        await client.connect();
 
     }
     finally {
